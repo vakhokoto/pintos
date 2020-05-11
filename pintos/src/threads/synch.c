8e4e26a0b431a3940770cc9aa502f6e66aaffaf8
vakhokoto <v.kotoreishvili@gmail.com>
2020-05-11 22:08:16 +0400

    some change but not working

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -335,7 +335,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
 
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  list_insert_ordered (&cond->waiters, &waiter.elem, list_less, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
@@ -356,9 +356,17 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (!intr_context ());
   ASSERT (lock_held_by_current_thread (lock));
 
-  if (!list_empty (&cond->waiters))
+  struct thread *temp = NULL;
+
+  if (!list_empty (&cond->waiters)){
+    temp = list_entry (list_front (&cond->waiters), struct thread, elem);
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
+  }
+
+  if(temp != NULL && temp->priority > thread_current()->priority) {
+    thread_yield();
+  }
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
