6d75f4c0eda5ca37684c08e3578927d20535494b
enqidu <dachikurcxalia@gmail.com>
2020-05-31 16:26:45 +0400

    page fault handling

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -4,6 +4,9 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "vm/page.h"
+#include "vm/frame.h"
+#include "vm/swap.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -148,6 +151,14 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+  if (!not_present){
+     struct hash swap_table = thread_current()->swap_table;
+     swap_idx_t swap_idx; // = get_swap_idx(&swap_table,fault_addr);
+     uint8_t* kaddr =  swap_get(swap_idx);
+      
+  }
+
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
@@ -158,4 +169,7 @@ page_fault (struct intr_frame *f)
           user ? "user" : "kernel");
   kill (f);
 }
+
+
+
 
